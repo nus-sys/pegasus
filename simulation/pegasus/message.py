0d20261e42e77a998c37e2b8419b564b50993cc9
Jialin Li <lijl@cs.washington.edu>
2018-02-02 16:07:15 -0800

    added send_time to messages

--- simulation/pegasus/message.py
+++ simulation/pegasus/message.py
@@ -7,7 +7,21 @@ class Message(object):
     Abstract class, representing a generic network message.
     """
     def __init__(self, length):
-        self._length = length
+        self.length = length
+        self.send_time = 0
 
+    @property
     def length(self):
         return self._length
+
+    @length.setter
+    def length(self, length):
+        self._length = length
+
+    @property
+    def send_time(self):
+        return self._send_time
+
+    @send_time.setter
+    def send_time(self, send_time):
+        self._send_time = send_time
