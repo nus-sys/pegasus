f58e2c02cd2903e291424519e8ed303fd6343a2b
Jialin Li <lijl@cs.washington.edu>
2018-02-02 16:06:47 -0800

    process_message now takes a time argument

--- simulation/pegasus/application.py
+++ simulation/pegasus/application.py
@@ -6,8 +6,8 @@ the simulator.
 class Application(object):
     """
     Abstract class, representing a generic application running on
-    a node. Subclass of Application should implement ``_execute``
-    and ``_process_message``.
+    a node. Subclass of ``Application`` should implement
+    ``_execute`` and ``_process_message``.
     """
     def __init__(self, node):
         self._node = node
@@ -21,11 +21,11 @@ class Application(object):
     def _execute(self, end_time):
         raise NotImplementedError
 
-    def process_message(self, message):
+    def process_message(self, message, time):
         """
         Process a single message
         """
-        self._process_message(message)
+        self._process_message(message, time)
 
-    def _process_message(self, message):
+    def _process_message(self, message, time):
         raise NotImplementedError
