1c17489df71ec59aa1634efd9abcc16b1e0408fd
Jialin Li <lijl@cs.washington.edu>
2018-02-04 17:27:01 -0800

    fixed a bug

--- simulation/pegasus/applications/nullrpc.py
+++ simulation/pegasus/applications/nullrpc.py
@@ -12,7 +12,7 @@ MESSAGE_INTERVAL = 5
 class NullRPC(pegasus.application.Application):
     def __init__(self):
         super().__init__()
-        self._next_send_time = 0
+        self._next_send_time = MESSAGE_INTERVAL
         self._sent_messages = 0
         self._received_messages = 0
 
@@ -20,10 +20,10 @@ class NullRPC(pegasus.application.Application):
         """
         Send message to randomly selected node every ``MESSAGE_INTERVAL``
         """
-        while self._next_send_time < end_time:
+        while self._next_send_time <= end_time:
             dest_node = self._local_node
             # Do not send to itself
-            while dest_node is self._remote_nodes:
+            while dest_node is self._local_node:
                 dest_node = random.choice(self._remote_nodes)
             msg = pegasus.message.Message(MESSAGE_LENGTH)
             self._local_node.send_message(msg, dest_node, self._next_send_time)
