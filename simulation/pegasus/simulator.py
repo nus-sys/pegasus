9d8f8d4da6e570521a86792b44d56f2c49050f93
Jialin Li <lijl@cs.washington.edu>
2018-02-04 17:28:16 -0800

    top level simulator

---
+++ simulation/pegasus/simulator.py
@@ -0,0 +1,40 @@
+"""
+simulator.py: Top level simulator.
+"""
+
+import copy
+
+import pegasus.node
+import pegasus.application
+from pegasus.config import *
+
+class Simulator(object):
+    def __init__(self):
+        self._nodes = []
+
+    def setup(self, n_racks, n_nodes_per_rack, app):
+        assert n_racks > 0
+        assert n_nodes_per_rack > 0
+        # Setup nodes
+        for i in range(n_racks):
+            rack = pegasus.node.Rack(i)
+            for j in range(n_nodes_per_rack):
+                node = pegasus.node.Node(rack, j)
+                self._nodes.append(node)
+
+        # Setup apps
+        for node in self._nodes:
+            node_app = copy.deepcopy(app)
+            node_app.register_nodes(node, self._nodes)
+            node.register_app(node_app)
+
+    def run(self, duration):
+        """
+        Run the simulator for ``duration`` usecs.
+        """
+        timer = MIN_PROPG_DELAY
+        while timer <= duration:
+            for node in self._nodes:
+                node.run(timer)
+
+            timer += MIN_PROPG_DELAY
---
+++ simulation/tests/test_simulator.py
@@ -0,0 +1,35 @@
+"""
+test_simulator.py: Unit tests for the top level simulator.
+"""
+
+import unittest
+import pegasus.simulator
+import pegasus.applications.nullrpc as nullrpc
+from pegasus.config import *
+
+class BasicTest(unittest.TestCase):
+    def setUp(self):
+        self.simulator = pegasus.simulator.Simulator()
+        self.simulator.setup(4, 4, nullrpc.NullRPC())
+
+    def test_basic(self):
+        assert (MIN_PROPG_DELAY // nullrpc.MESSAGE_INTERVAL) * PKT_PROC_LTC < MIN_PROPG_DELAY
+        n_rounds = 4
+        msg_sent_sum = 0
+        msg_received_sum = 0
+        timer = n_rounds * nullrpc.MESSAGE_INTERVAL
+
+        self.simulator.run(timer)
+
+        # Process remaining messages
+        timer += (2 * MIN_PROPG_DELAY + len(self.simulator._nodes) * PKT_PROC_LTC)
+        for node in self.simulator._nodes:
+            node.process_messages(timer)
+
+        for node in self.simulator._nodes:
+            msg_sent_sum += node._app._sent_messages
+            msg_received_sum += node._app._received_messages
+
+        expected_msgs = n_rounds * len(self.simulator._nodes)
+        self.assertEqual(msg_sent_sum, expected_msgs)
+        self.assertEqual(msg_sent_sum, msg_received_sum)
