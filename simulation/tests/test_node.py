b31b5ca6700c30e01b63770e7f6b68166dcf2ff1
Jialin Li <lijl@cs.washington.edu>
2018-02-11 11:25:15 -0800

    added multi-proc test

--- simulation/tests/test_node.py
+++ simulation/tests/test_node.py
@@ -11,12 +11,13 @@ import pegasus.param as param
 class TestApp(application.Application):
     def __init__(self):
         super().__init__()
+        self.received_msgs = 0
 
     def execute(self, end_time):
         pass
 
     def process_message(self, message, time):
-        pass
+        self.received_msgs += 1
 
 
 class TwoNodesTest(unittest.TestCase):
@@ -24,9 +25,10 @@ class TwoNodesTest(unittest.TestCase):
         self.rack = node.Rack()
         self.node_a = node.Node(self.rack)
         self.node_b = node.Node(self.rack)
-        self.app = TestApp()
-        self.node_a.register_app(self.app)
-        self.node_b.register_app(self.app)
+        self.app_a = TestApp()
+        self.app_b = TestApp()
+        self.node_a.register_app(self.app_a)
+        self.node_b.register_app(self.app_b)
         self.rack.add_node(self.node_a)
         self.rack.add_node(self.node_b)
 
@@ -36,9 +38,10 @@ class TwoNodesTest(unittest.TestCase):
         self.assertEqual(len(self.node_b._message_queue), 1)
         arrv_time = self.node_b._message_queue[0].time
         self.node_b.run(arrv_time)
-        self.assertEqual(len(self.node_b._message_queue), 1)
+        self.assertEqual(self.app_b.received_msgs, 0)
         self.node_b.run(arrv_time+param.MAX_PKT_PROC_LTC+1)
         self.assertEqual(len(self.node_b._message_queue), 0)
+        self.assertEqual(self.app_b.received_msgs, 1)
 
     def test_multiple_messages(self):
         msg = message.Message(1024)
@@ -50,18 +53,51 @@ class TwoNodesTest(unittest.TestCase):
         self.assertEqual(len(self.node_b._message_queue), 2)
         timer = self.node_a._message_queue[0].time + param.MAX_PKT_PROC_LTC
         self.node_a.run(timer)
-        self.assertEqual(len(self.node_a._message_queue), 1)
+        self.assertEqual(self.app_a.received_msgs, 1)
         timer += param.MAX_PKT_PROC_LTC + (param.MAX_PROPG_DELAY - param.MIN_PROPG_DELAY)
         self.node_a.run(timer)
         self.assertEqual(len(self.node_a._message_queue), 0)
+        self.assertEqual(self.app_a.received_msgs, 2)
 
         timer = self.node_b._message_queue[0].time + param.MAX_PKT_PROC_LTC
         self.node_b.run(timer)
-        self.assertEqual(len(self.node_b._message_queue), 1)
+        self.assertEqual(self.app_b.received_msgs, 1)
         timer += param.MAX_PKT_PROC_LTC + (param.MAX_PROPG_DELAY - param.MIN_PROPG_DELAY)
         self.node_b.run(timer)
         self.assertEqual(len(self.node_b._message_queue), 0)
+        self.assertEqual(self.app_b.received_msgs, 2)
 
+
+class MultiProcsTest(unittest.TestCase):
+    def setUp(self):
+        rack = node.Rack()
+        self.server_a = node.Node(rack, 1)
+        self.server_b = node.Node(parent=rack, id=2, nprocs=4)
+        self.app_a = TestApp()
+        self.app_b = TestApp()
+        self.server_a.register_app(self.app_a)
+        self.server_b.register_app(self.app_b)
+
+    def test_basic(self):
+        msg = message.Message(1024)
+        for _ in range(8):
+            self.server_a._message_queue.add(node.QueuedMessage(msg, 0))
+            self.server_b._message_queue.add(node.QueuedMessage(msg, 0))
+
+        self.server_a.run(param.MIN_PKT_PROC_LTC-1)
+        self.server_b.run(param.MIN_PKT_PROC_LTC-1)
+        self.assertEqual(self.app_a.received_msgs, 0)
+        self.assertEqual(self.app_b.received_msgs, 0)
+
+        self.server_a.run(param.MAX_PKT_PROC_LTC)
+        self.server_b.run(param.MAX_PKT_PROC_LTC)
+        self.assertEqual(self.app_a.received_msgs, 1)
+        self.assertEqual(self.app_b.received_msgs, 4)
+
+        self.server_a.run(2*param.MAX_PKT_PROC_LTC)
+        self.server_b.run(2*param.MAX_PKT_PROC_LTC)
+        self.assertEqual(self.app_a.received_msgs, 2)
+        self.assertEqual(self.app_b.received_msgs, 8)
 
 class TwoRacksTest(unittest.TestCase):
     def setUp(self):
