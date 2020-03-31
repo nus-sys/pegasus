04e5515f504e4797ad5fb68e6e42e4540a23c6c0
Jialin Li <lijl@comp.nus.edu.sg>
2020-03-31 17:54:58 +0800

    remove abort() call

--- emulation/logger.cc
+++ emulation/logger.cc
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "logger.h"
+
+#include <logger.h>
 
 void panic(const char *msg)
 {
     printf("PANIC: %s\n", msg);
-    abort();
     exit(1);
 }
